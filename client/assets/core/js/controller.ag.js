26dc8f3e087e30f4ada8087cf83abcee412218d9
nisnaker <nisnaker@gmail.com>
2015-11-16 13:53:53 +0800

    login box css

--- client/assets/core/js/controller.ag.js
+++ client/assets/core/js/controller.ag.js
@@ -8,5 +8,30 @@
 			$scope.user = {email: '', passwd: '', passwd2: ''};
 
 			$('.login-box a').fancybox();
+
+			// 检测email格式
+			$scope.check_email = function () {
+				return /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test($scope.user.email);
+			}
+
+			// 按钮状态
+			$scope.check_btn = function (btn) {
+				if('reg' == btn)
+					return $scope.user.email && $scope.user.passwd && $scope.user.passwd == $scope.user.passwd2 && $scope.check_email();
+				else if ('login' == btn)
+					return $scope.user.email && $scope.user.passwd && $scope.check_email();
+			}
+
+			$scope.reg = function () {
+				if(!$scope.check_btn('reg'))
+					return;
+				l('reg')
+			}
+
+			$scope.login = function () {
+				if(!$scope.check_btn('login'))
+					return;
+				l('login')
+			}
 		}]);
 })();
